81775b0c4cee89d587421f730c75581f477616c7
unknown <bdagg@csumb.edu>
2020-03-24 21:15:49 -0700

    changed query

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -61,7 +61,7 @@ export class ResultList extends Component {
         console.log('query in func =  ' + query);
         var baseURLDev = 'http://localhost:5000';
         var baseURL = 'https://store-app-dagg.herokuapp.com'
-        axios.get(baseURL + query)
+        axios.get(query)
         .then(res => {
             console.log(res);
             
